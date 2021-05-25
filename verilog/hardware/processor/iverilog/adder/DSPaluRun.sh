aea03d77dd2a1f291a9108b8a8f58b58de2e9097
leacog <mikael.cognell@gmail.com>
2021-05-25 18:33:24 +0000

    transferring alu test

--- verilog/hardware/processor/iverilog/adder/DSPaluRun.sh
+++ verilog/hardware/processor/iverilog/adder/DSPaluRun.sh
@@ -1,2 +1,5 @@
+rm DSPalu
+rm DSPalu.vcd
+
 iverilog -o DSPalu DSP_alu.v DSPalu_sim.v DSPadder.v DSPsubtractor.v alu_control.v SB_MAC16.v
 ./DSPalu
--- verilog/hardware/processor/iverilog/adder/aluRun.sh
+++ verilog/hardware/processor/iverilog/adder/aluRun.sh
@@ -1,2 +1,4 @@
+rm alu
+rm alu.vcd
 iverilog -o alu alu.v alu_sim.v alu_control.v
 ./alu
