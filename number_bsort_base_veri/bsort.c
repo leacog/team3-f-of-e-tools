e3938ad1384fcd1ccc0f0b6c648b2e3e50dcf345
Huoyongtony <46124508+Huoyongtony@users.noreply.github.com>
2021-06-01 10:36:34 +0100

    new test

---
+++ number_bsort_base_veri/bsort.c
@@ -0,0 +1,62 @@
+#include "sf-types.h"
+#include "bsort-input.h"
+#include "string.h"
+#include "softwareblink.h"
+
+uint bsort_input_copied[20];
+void run(void)
+{
+        int i;
+        int maxindex = bsort_input_len - 1;
+
+       
+        memcpy(&bsort_input_copied, &bsort_input, bsort_input_len);
+
+        while (maxindex > 0)
+        {
+                for (i = 0; i < maxindex; i++)
+                {
+                        if (bsort_input_copied[i] > bsort_input_copied[i+1])
+                        {
+                                /*              swap            */
+                                bsort_input_copied[i] ^= bsort_input_copied[i+1];
+                                bsort_input_copied[i+1] ^= bsort_input_copied[i];
+                                bsort_input_copied[i] ^= bsort_input_copied[i+1];
+                        }
+                }
+
+                maxindex--;
+        }
+}
+
+void blink(void)
+{
+	*gDebugLedsMemoryMappedRegister = 0xFF;
+
+	/*
+	 *	Spin
+	 */
+	for (int j = 0; j < kSpinDelay; j++);
+
+	*gDebugLedsMemoryMappedRegister = 0x00;
+
+	/*
+	 *	Spin
+	 */
+	for (int j = 0; j < kSpinDelay; j++);
+}
+
+int main(void)
+{
+        while (1)
+        {
+                *gDebugLedsMemoryMappedRegister = 0xFF;
+                run();
+                *gDebugLedsMemoryMappedRegister = 0x00;
+
+				for (int i = 0; i < 20; i++) {
+					*gDebugLedsMemoryMappedRegister = bsort_input_copied[i];
+				}
+        }
+        return 0;
+}
