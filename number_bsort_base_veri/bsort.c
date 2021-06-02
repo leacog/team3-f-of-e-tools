898f15a39b191f13dc11416cd50a8d887885e7d4
Huoyongtony <46124508+Huoyongtony@users.noreply.github.com>
2021-06-02 11:43:16 +0100

    Update bsort.c

--- number_bsort_base_veri/bsort.c
+++ number_bsort_base_veri/bsort.c
@@ -3,42 +3,31 @@
 #include "string.h"
 #include "softwareblink.h"
 
-void run(void)
+
+
+
+int
+main(void)
 {
-        int i;
-        int maxindex = bsort_input_len - 1;
+	int i;
+	int maxindex = bsort_input_len - 1;
 
+	while (maxindex > 0)
+	{
+		for (i = 0; i < maxindex; i++)
+		{
+			if (bsort_input[i] > bsort_input[i + 1])
+			{
+				/*		swap		*/
+				bsort_input[i] ^= bsort_input[i + 1];
+				bsort_input[i + 1] ^= bsort_input[i];
+				bsort_input[i] ^= bsort_input[i + 1];
+			}
+		}
+		*gDebugLedsMemoryMappedRegister = bsort_input[maxindex];
+		maxindex--;
+	}
+	
 
-        while (maxindex > 0)
-        {
-                for (i = 0; i < maxindex; i++)
-                {
-					if (bsort_input[i] > bsort_input[i+1])
-                        {
-                                /*              swap            */
-                                bsort_input[i] ^= bsort_input[i+1];
-                                bsort_input[i+1] ^= bsort_input[i];
-                                bsort_input[i] ^= bsort_input[i+1];
-                        }
-                }
-                maxindex--;
-        }
-}
-
-
-int main(void)
-{
-        while (1)
-        {
-                 *gDebugLedsMemoryMappedRegister = 0xFF;
-                run();
-                *gDebugLedsMemoryMappedRegister = 0x00;
-
-				for (int i = 20; i < 40; i++) {
-
-					*gDebugLedsMemoryMappedRegister = bsort_input[i];
-
-				}
-        }
-        return 0;
+	return 0;
 }
