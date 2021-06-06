f2a9f4ddff591594d2be58114c839d82ecc460e5
leacog <mikael.cognell@gmail.com>
2021-06-06 11:47:54 +0100

    first attempt at lowpass filter

--- lowPass/template.c
+++ lowPass/template.c
@@ -1,18 +1,33 @@
 #include "lowpass-input.h"
 
-int
-main(void)
+void sendSerial(unsigned char inputByte)
 {
-	int i;
-	int tailLenght = 1;
-	long output[lowpass_input_len];
-	for (i = 0; i < lowpass_input_len; i++) {
-		for(int j = 0; j < tailLenght; j++){
-			output[i] += lowpass_input[i-j];
-		} 
-		if(tailLenght < maxTailLenght) {
-			tailLenght++;
+	*gDebugLedsMemoryMappedRegister = 0xFF;
+	for (int j = 0; j < kSpinDelay*8; j++);
+
+	unsigned char bits = inputByte;
+	for(int i = 0; i < 8; i++) {
+		*gDebugLedsMemoryMappedRegister = 0x0F; //Value to indicate new read for verilator
+		if(bits & 0x01) {
+			*gDebugLedsMemoryMappedRegister = 0xFF;
+		} else {
+			*gDebugLedsMemoryMappedRegister = 0x00;
 		}
+		bits = (bits >> 1);
+		for (int j = 0; j < kSpinDelay; j++);
+	}
+	*gDebugLedsMemoryMappedRegister = 0x00;
+}
+
+int main(void)
+{
+	float output[lowpass_input_len - NUM_TAPS];
+
+	for (int i = 0; i < lowpass_input_len - NUM_TAPS; i++) {
+		for(int j = 0; j < NUM_TAPS; j++){
+			output[i] += (lowpass_input_1kHz[i-j] + lowpass_input_16kHz[i-j]) * firCoeffs[j];
+			sendSerial( (unsigned char)output[i] );
+		} 
 	}
 	return 0;
 }
