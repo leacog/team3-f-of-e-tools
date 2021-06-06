ffbd5370bc29ff91e8715d7ab32fa138d4196770
sri-cambridge <sg875@cam.ac.uk>
2021-06-06 11:32:58 +0000

    updated testbench

--- bubble_tb/bsort.c
+++ bubble_tb/bsort.c
@@ -3,51 +3,47 @@
 #include "string.h"
 #include "softwareblink.h"
 
-
-
-
-int
-main(void)
+void run()
 {
 	int i;
 	int maxindex = bsort_input_len - 1;
 	*gDebugLedsMemoryMappedRegister = 0x00;
+	uchar* input = bsort_input;
 
 	while (maxindex > 0)
 	{
 		for (i = 0; i < maxindex; i++)
 		{
-			if (bsort_input[i] > bsort_input[i + 1])
+			if (input[i] > input[i + 1])
 			{
 				/*		swap		*/
-				bsort_input[i] ^= bsort_input[i + 1];
-				bsort_input[i + 1] ^= bsort_input[i];
-				bsort_input[i] ^= bsort_input[i + 1];
+				input[i] ^= input[i + 1];
+				input[i + 1] ^= input[i];
+				input[i] ^= input[i + 1];
 			}
 		}
 		//for (int j = 0; j < kSpinDelay; j++);
 		//for (int j = 0; j < kSpinDelay; j++);
 		//for (int j = 0; j < kSpinDelay; j++);
-
 		//for (int j = 1; j < 0x100; j++) {
 
-		if (bsort_input[maxindex] == bsorted[maxindex]) { *gDebugLedsMemoryMappedRegister = 0xFF; }
+		if (input[maxindex] == bsorted[maxindex]) { *gDebugLedsMemoryMappedRegister = 0xFF; }
 		else {*gDebugLedsMemoryMappedRegister = 0x00; };	
 		//for (int m = 0; m < kSpinDelay; m++);
 		//*gDebugLedsMemoryMappedRegister = bsort_input[maxindex];
 		
 		maxindex--;
 	}
-	
+	for (int j = 0; j < kSpinDelay; j++);
+	for (int j = 0; j < kSpinDelay; j++);
+}
 
+int main(void)
+{
+	while (1)
+	{
+		*gDebugLedsMemoryMappedRegister = 0x00;
+		run();
+	}
 	return 0;
-	for (int j = 0; j < kSpinDelay; j++);
-	for (int j = 0; j < kSpinDelay; j++);
-	for (int j = 0; j < kSpinDelay; j++);
-	for (int j = 0; j < kSpinDelay; j++);
-        for (int j = 0; j < kSpinDelay; j++);
-        for (int j = 0; j < kSpinDelay; j++);
-	for (int j = 0; j < kSpinDelay; j++);
-        for (int j = 0; j < kSpinDelay; j++);
-        for (int j = 0; j < kSpinDelay; j++);
 }
